a72385337b4008bbff9f1f6d0f2dcfb9c6d7c38d
seanj29 <seanotto29@gmail.com>
2023-12-14 20:33:39 +0000

    hard reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,14 @@
-"use client"
-import { useState, useEffect } from "react"
+
 
 export default function Home() {
 
-  // type RecipeReponse = [{
-  //   id: string,
-  //   ingredients: string,
-  //   title: string,
-  //   uuid: string,
-  // }]
-  type PersonType = {
-    result:
-    [{
-  name: 
-    {
-    first: string
-    }
-  ,
-  id:
-    {
-    value: Number
-    }
-  }
-]
-
-  }
-
-  const [query, setQuery] = useState("")
-  const [recipes, setRecipes] = useState<PersonType>() 
+  
 
 
-  useEffect(() => {
-
-    const fetchRecipes = async () =>{
-
-      const response = await fetch("https://randomuser.me/api/?results=50")
-      const data = await response.json()
-      setRecipes(data)
-    }
-
-    fetchRecipes()
-  }, [])
 
   return (
     <div>
-   <input placeholder="Enter Recipe Name" onChange= {(e) => setQuery(e.target.value)} />
-   {
-   
-    recipes?.result
-
-    .filter((recipe) => 
-
-   {
-   if(recipe.name.first.toLowerCase().includes(query.toLowerCase())){
-    return recipe.name.first
-   }
-   }
-   
-   )
-
-   
-   .map((recipe, i) => 
-   <p key={recipe.id.value && i}>{recipe.name.first}</p>
-   )
-}
+ <p> Hiiiiiiiiii....! </p>
    </div>
   )
 }
