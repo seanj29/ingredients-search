f96046ec8928583d1560f7ebbf7249c4dcc3bd53
seanj29 <seanotto29@gmail.com>
2023-12-18 12:46:28 +0000

    route api set up

---
+++ src/app/api/route.ts
@@ -0,0 +1,20 @@
+
+import { sql } from '@vercel/postgres';
+import { Recipes } from "../../db/definitions"
+import { type NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest){
+  const searchParams = request.nextUrl.searchParams
+  const query = searchParams.get('query')
+  const page = searchParams.get('page')
+  const size = searchParams.get('size')
+
+  const sizeasNum = parseInt(size || "25")
+  const pageasNum = parseInt(page ||"1")
+
+  const data = await sql`SELECT name FROM recipes WHERE ${query} % ANY(STRING_TO_ARRAY(name, ' ')) ORDER BY SIMILARITY(name, ${query}) DESC OFFSET ${pageasNum * sizeasNum} FETCH NEXT ${sizeasNum} ROW ONLY`;  
+    
+  
+
+    return Response.json(data.rows)
+  }
